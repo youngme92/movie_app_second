6907acb0fa382b8a8f1598735b6e6d9cf12be67a
youngme92 <youngme992@gmail.com>
2021-08-22 23:17:49 +0900

    #2.4 Protection with PropTypes (08:51)

--- src/example.js
+++ src/example.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-function Example({ name, image }) {
+function Example({ name, image, rating }) {
     return (
         <div>
             <h2>This is {name}</h2>
+            <h3>{rating} / 5</h3>
             <img src={image} alt={name}></img>
         </div>
     )
