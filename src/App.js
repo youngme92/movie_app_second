52fcb528ca034466d9d2cf681aaa9522b6c0bd2b
youngme92 <youngme992@gmail.com>
2021-08-31 22:52:30 +0900

    #3 STATE 완료

--- src/App.js
+++ src/App.js
@@ -1,58 +1,31 @@
 import Food from './example';
 import propTypes from 'prop-types';
+import React from 'react';
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 3
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 2
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 5
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor!!');
   }
-];
+  state = {
+    isLoading: true
+  }
+  componentDidMount() {
+    console.log("componentDidMount!!")
+    setTimeout(() => {
+      this.setState(current => ({ isLoading: !current.isLoading }));
+    }, 3000)
+  }
 
-Food.propTypes = {
-  name: propTypes.string.isRequired,
-  image: propTypes.string,
-  rating: propTypes.number.isRequired
-}
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(item => (
-        <Food key={item.id} name={item.name} image={item.image} rating={item.rating} />)
-      )}
-    </div>
-  );
+  render() {
+    const { isLoading } = this.state;
+    console.log('render!!')
+    return (
+      <div>
+        <h1>{isLoading ? 'Loading...' : 'show moives'}</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
