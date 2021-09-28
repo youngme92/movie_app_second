b4d891c71db2fcb6d8c4982ec73f0dc44c5e089e
youngme92 <youngme992@gmail.com>
2021-09-28 23:15:26 +0900

    #6.2 Building the Navigation (05:36)

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
-// import Food from './example';
-// import propTypes from 'prop-types';
-import axios from 'axios';
 import React from 'react';
-import Movies from './Movies';
-import './App.css';
+import Home from './routes/Home';
+import About from './routes/About';
+import { HashRouter, BrowserRouter, Route } from 'react-router-dom';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('constructor!!');
-  }
-  state = {
-    isLoading: true,
-    movies: []
-  }
-  getMoives = async () => {
-    const { data: { data: { movies } } } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
-    this.setState({ movies, isLoading: false });
-  }
-  componentDidMount() {
-    this.getMoives();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <div className="container">
-        {isLoading ? (
-          <div className="loader">
-            <h2>isLoading...</h2>
-          </div>
-        ) : movies.map(movies => (
-          <div key={movies.id} className="movies">
-            <Movies
-              key={movies.id}
-              id={movies.id}
-              year={movies.year}
-              title={movies.title}
-              summary={movies.summary}
-              youtubeUrl={movies.yt_trailer_code}
-              poster={movies.medium_cover_image}
-              genres={movies.genres} />
-          </div>
-        ))}
-      </div>
-    )
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" exact={true} component={About} />
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import React from 'react';
+import Movies from '../components/Movies';
+import './Home.css';
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log('constructor!!');
+    }
+    state = {
+        isLoading: true,
+        movies: []
+    }
+    getMoives = async () => {
+        const { data: { data: { movies } } } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+        this.setState({ movies, isLoading: false });
+    }
+    componentDidMount() {
+        this.getMoives();
+    }
+
+    render() {
+        const { isLoading, movies } = this.state;
+        return (
+            <div className="container">
+                {isLoading ? (
+                    <div className="loader">
+                        <h2>isLoading...</h2>
+                    </div>
+                ) : movies.map(movies => (
+                    <div key={movies.id} className="movies">
+                        <Movies
+                            key={movies.id}
+                            id={movies.id}
+                            year={movies.year}
+                            title={movies.title}
+                            summary={movies.summary}
+                            youtubeUrl={movies.yt_trailer_code}
+                            poster={movies.medium_cover_image}
+                            genres={movies.genres} />
+                    </div>
+                ))}
+            </div>
+        )
+    }
+}
+
+export default Home;
